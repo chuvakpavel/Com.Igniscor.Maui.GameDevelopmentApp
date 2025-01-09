14fe3211ebd92e0b9cc2323231dde1fa2252e63b
chuvakpavel <chuvak.pavel@gmail.com>
2025-01-09 10:49:16 +0300

    Creat project

---
+++ HungerHydra/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HungerHydra
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
